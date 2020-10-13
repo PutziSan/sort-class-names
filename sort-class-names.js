33d2b77844777edb389f14c48632d59b787719ec
PutziSan <putzisan@putzisan.de>
2020-10-13 11:54:38 +0200

    add new compiled version

--- sort-class-names.js
+++ sort-class-names.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __spreadArrays = (this && this.__spreadArrays) || function () {
     for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
     for (var r = Array(s), k = 0, i = 0; i < il; i++)
@@ -139,5 +150,10 @@ function sort(srcText) {
 }
 var fileName = process.argv[2];
 var oldCodeContent = fs.readFileSync(fileName, "utf8");
-var newCodeContent = sort(prettier_1.format(oldCodeContent, { parser: "babel" }));
+var config = {};
+try {
+    config = require("./prettier.config");
+}
+catch (e) { }
+var newCodeContent = sort(prettier_1.format(oldCodeContent, __assign(__assign({}, config), { parser: "babel" })));
 fs.writeFileSync(fileName, newCodeContent, "utf8");
